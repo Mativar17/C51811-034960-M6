eaf00c69cc75f2ae672df1a76bb41a39cdc4d8de
Matias Vargas <mati.vargasg@icloud.com>
2021-08-08 23:22:01 -0400

    first commit

--- Actividades/AC01/E07/A1E7.rb
+++ Actividades/AC01/E07/A1E7.rb
@@ -4,4 +4,7 @@ Mostrar un mensaje indicando si el número tiene uno o dos dígitos.
 (Tener en cuenta qué condición debe cumplirse para tener dos dígitos un número entero)    
 =end
 
-num = gets.to_i
+puts "Ingrese un número: "
+num = gets.to_s.length
+
+puts "El número ingresado tiene #{num} digitos"
