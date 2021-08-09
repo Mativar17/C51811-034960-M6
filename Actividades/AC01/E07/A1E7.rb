bfddbc064863f066989c8eb030af3bb3cab8e161
Matias Vargas <mati.vargasg@icloud.com>
2021-08-08 22:12:41 -0400

    added activities

--- Actividades/AC01/E07/A1E7.rb
+++ Actividades/AC01/E07/A1E7.rb
@@ -3,3 +3,5 @@ Se ingresa por teclado un número positivo de uno o dos dígitos (1.99)
 Mostrar un mensaje indicando si el número tiene uno o dos dígitos.
 (Tener en cuenta qué condición debe cumplirse para tener dos dígitos un número entero)    
 =end
+
+num = gets.to_i
