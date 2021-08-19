45e21c627d2cec92be8ab640f69a21e163b77550
Mativar17 <matyvargasg14@gmail.com>
2021-08-19 19:52:43 -0400

    added polimorfismo

--- polimorfismo.rb
+++ polimorfismo.rb
@@ -1,6 +1,6 @@
 class Word
     def mostrarExtension
-        puts ".word"
+        puts ".docx"
     end
 end
 
@@ -14,4 +14,10 @@ class Excel
     def mostrarExtension
         puts ".xlsx"
     end
+end
+
+programas = [Word.new, PowerPoint.new, Excel.new, PowerPoint.new, Word.new]
+
+programas.each do |programa|
+    programa.mostrarExtension
 end
