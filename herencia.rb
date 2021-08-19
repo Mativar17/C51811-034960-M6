779ada6f50f4af99364f3b7fabd113b28bcaf073
Mativar17 <matyvargasg14@gmail.com>
2021-08-19 19:46:46 -0400

    polimorfismo

--- herencia.rb
+++ herencia.rb
@@ -8,9 +8,37 @@ class Persona
     end
 end
 
-class Estudiante
+class EstudianteSecundaria < Persona
+        attr_accesor :calificacion
+    def initialize(calificacion1:, calificacion2:, calificacion3:, **options)
+        #super(**options)
+        @calificacion1 = calificacion1
+        @calificacion2 = calificacion2
+        @calificacion3 = calificacion3
+    end
+
+    def calculamiPromedio
+        promedio = ()
+
+    end
+end
+
+class EstudianteUniversitaria < Persona
+        attr_accesor :calificacion
+    def initialize(calificacion1:, calificacion2:, calificacion3:, **options)
+        super(**options)
+        @calificacion1 = calificacion1 #Esta nota vale un 30%
+        @calificacion2 = calificacion2 #Esta nota vale un 30%
+        @calificacion3 = calificacion3 #Esta nota vale un 40%
+    end
+
+    def calculamiPromedio
+        promedio = (@calificacion1 * 0.3) + (calificacion2 * 0.3) + (calificacion3 * 0.4)
+    end
 
 end
 
-juan = Estudiante.new(rut: '111-1', nombre_completo:'Juan Perez', edad: 25, sexo: 'hombre', domicilio: 'main 123')
-puts juan.nombre_completo
+secundario1 = EstudianteSecundaria.new(calificacion1: 6.5, rut: '111-1', nombre_completo: 'Juan Vargas', )
+universitario1 = EstudianteUniversitaria.new(calificacion1: 3, calificacion2: 5, calificacion3: 7, rut: '222-2', nombre_completo: 'Juan Vargas', )
+puts "El estudiante de secundaria #{secundario1.nombre_completo} tiene un promedio de #{secundario1.calculamiPromedio}."
+puts "El estudiante de universidad #{universitario1.nombre_completo} tiene un promedio de #{universitario1.calculamiPromedio}"
---
+++ polimorfismo.rb
@@ -0,0 +1,17 @@
+class Word
+    def mostrarExtension
+        puts ".word"
+    end
+end
+
+class PowerPoint
+    def mostrarExtension
+        puts ".pptx"
+    end
+end
+
+class Excel
+    def mostrarExtension
+        puts ".xlsx"
+    end
+end
